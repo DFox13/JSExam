14f6bd4f4eb16aeb663c6db54fd0da7ff635db91
v.zhelanov <syz.vadim@gmail.com>
2024-12-09 19:41:47 +0300

    Доработаны функции отображения героев

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
+const actors = ['wolf', 'lamb', 'cabbage'];
+
 document.querySelector('.board').addEventListener('click', function(){
     document.querySelector('.board').classList.toggle('board-initial');
     document.querySelector('.board').classList.toggle('board-final');
 });
-const actors = [];
-
 draw();
+
 function draw(){
-    actors.forEach(actor => {
-        document.querySelector('left').appendChild(createActor(actor));
+    document.querySelector('.left').innerHTML = '';
+    document.querySelector('.right').innerHTML = '';
+
+    leftBank.forEach(actor => {
+        document.querySelector('.left').appendChild(createActor(actor));
+    });
+
+    rightBank.forEach(actor => {
+        document.querySelector('.right').appendChild(createActor(actor));
     });
 }
 
 function createActor(actor){
     const actorItem = document.createElement('div');
-    actorItem.className = actor;
+    actorItem.className = 'actor ' + actor;
+
+    const actorImage = document.createElement('img');
+    actorImage.src = '/image/' + actor + '.webp';
+    actorImage.alt = actor;
+
+    actorItem.appendChild(actorImage);
 
     return actorItem;
 }
