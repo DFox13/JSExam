50a4712a50de2bd1dc25ce5b004cfec15bba4965
v.zhelanov <syz.vadim@gmail.com>
2024-12-09 20:21:33 +0300

    добавлена функция перемещения героев на другой берег

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const actors = ['wolf', 'lamb', 'cabbage'];
+let left = actors.slice();
+let onBoard = [];
+let right = [];
 
 document.querySelector('.board').addEventListener('click', function(){
     document.querySelector('.board').classList.toggle('board-initial');
@@ -10,11 +13,11 @@ function draw(){
     document.querySelector('.left').innerHTML = '';
     document.querySelector('.right').innerHTML = '';
 
-    leftBank.forEach(actor => {
+    left.forEach(actor => {
         document.querySelector('.left').appendChild(createActor(actor));
     });
 
-    rightBank.forEach(actor => {
+    right.forEach(actor => {
         document.querySelector('.right').appendChild(createActor(actor));
     });
 }
@@ -27,7 +30,33 @@ function createActor(actor){
     actorImage.src = '/image/' + actor + '.webp';
     actorImage.alt = actor;
 
+    actorImage.addEventListener('click', function(){
+        if (left.includes(actor)) {
+            left = left.filter(a => a !== actor);
+            right.push(actor);
+        } else if (right.includes(actor)) {
+            right = right.filter(a => a !== actor);
+            left.push(actor);
+        }
+        draw();
+    });
+
     actorItem.appendChild(actorImage);
 
     return actorItem;
+}
+
+
+function seat(character) {
+    if (character === 'wolf') {
+        left = left.filter(actor => actor !== 'wolf');
+        right.push('wolf');
+    } else if (character === 'lamb') {
+        left = left.filter(actor => actor !== 'lamb');
+        right.push('lamb');
+    } else if (character === 'cabbage') {
+        left = left.filter(actor => actor !== 'cabbage');
+        right.push('cabbage');
+    }
+    draw();
 }
